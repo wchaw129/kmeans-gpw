820f6a6e4032d0e82bea2d2a661c380799188a73
wchaw03 <w-chawar@wp.pl>
2025-12-19 17:12:53 +0100

    data loading methods implementation

--- src/loading_data.py
+++ src/loading_data.py
@@ -1,27 +1,39 @@
 import pandas as pd
 import requests
+from typing import Optional
+from datetime import datetime
+from io import StringIO
 
 def load_tickers_from_website(
-        url="https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=WIG",
-        headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-                "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7"
-            }):
+        url: str ="https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=WIG",
+        headers: Optional[dict[str, str]] = None,
+        save_to_file: bool = False,
+        file_name: str = None
+) -> pd.DataFrame | None:
+    
     """
     Pobiera listę spółek z tabeli na stronie Bankier.pl.
-
-    Kolumna Ticker_yf zawiera listę tickerów odpowiednią dla nazewnictwa przez yfinance.
+    Dodaje sufiks .WA do tickerów dla biblioteki yfinance.
     """
 
+    if headers == None:
+          headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+            "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7"
+        }
+          
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status() 
-        tables = pd.read_html(response.text, decimal=',', thousands=' ')
+        tables = pd.read_html(StringIO(response.text), decimal=',', thousands=' ')
 
         if len(tables) > 0:
             df = tables[0]
             print("Pobrano pomyślnie")
             df['Ticker_yf'] = df['Ticker'] + ".WA"
+            if save_to_file:
+                timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                df.to_csv(f'data/{file_name if file_name else f'tickers_{timestamp}'}.csv')
             return df
         else:
             print("Nie znaleziono żadnej tabeli na stronie.")
@@ -32,15 +44,22 @@ def load_tickers_from_website(
         return None
 
 
-def load_tickers_from_file(filepath = r'data\WIG_ticker_list.csv', **kwargs):
+load_tickers_from_website(save_to_file = True, file_name = 'asssaa')
+
+def load_tickers_from_file(
+        filepath: str = r'data\WIG_ticker_list.csv', 
+        sep: str = ';'
+) -> pd.DataFrame | None:
+
     """
     Ładuje tabelę z pliku CSV i przygotowuje je dla Yahoo Finance.
     Kolumna Ticker powinna zawierać tickery spółek w tabeli
 
     Kolumna Ticker_yf zawiera listę tickerów odpowiednią dla nazewnictwa przez yfinance
     """
+
     try:
-        df = pd.read_csv(filepath, sep = ';')
+        df = pd.read_csv(filepath, sep = sep)
         df['Ticker_yf'] = df['Ticker'] + ".WA"
         print('Pobrano pomyślnie')
         return df
